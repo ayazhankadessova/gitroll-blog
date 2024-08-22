d279aaee9255ecc12e870f12a8969917449157d6
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-22 09:03:35 +0500

    add: sorted blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { posts } from '#site/content'
 import { PostItem } from '@/components/post-item'
+import { sortPosts } from '@/lib/utils'
 
 export default async function BlogPage() {
-  const displayPosts = posts
+  const sortedPosts = sortPosts(posts.filter((post) => post.published))
   return (
     <div className='container max-w-4xl py-2 lg:py-3 px-0'>
       <div className='flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8'>
@@ -14,9 +15,9 @@ export default async function BlogPage() {
         </div>
       </div>
       <hr className='mt-8'></hr>
-      {displayPosts?.length > 0 ? (
+      {sortedPosts?.length > 0 ? (
         <ul className='flex flex-col'>
-          {displayPosts.map((post) => {
+          {sortedPosts.map((post) => {
             const { slug, date, title, description, tags } = post
             return (
               <li key={slug}>
