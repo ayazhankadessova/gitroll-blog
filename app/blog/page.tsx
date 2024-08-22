df4454b8dafdd3d24885a2e1c19988c3cd89f4e9
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-22 15:16:36 +0500

    add: new pages & pagination logic

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,8 +4,22 @@ import { PostItem } from '@/components/post-item'
 import { sortPosts } from '@/lib/utils'
 import '@/styles/mdx-style.css'
 
-export default async function BlogPage() {
+const POSTS_PER_PAGE = 2
+interface BlogPageProps {
+  filterParams: {
+    page?: string
+  }
+}
+
+export default async function BlogPage({ filterParams }: BlogPageProps) {
   const sortedPosts = sortPosts(posts.filter((post) => post.published))
+  const currentPage = Number(filterParams?.page) || 1
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1) + 1,
+    currentPage
+  )
+
   return (
     <div className='container max-w-4xl py-2 lg:py-3 px-0'>
       <div className='flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8'>
@@ -16,9 +30,9 @@ export default async function BlogPage() {
         </div>
       </div>
       <hr className='mt-8'></hr>
-      {sortedPosts?.length > 0 ? (
+      {displayPosts?.length > 0 ? (
         <ul className='flex flex-col'>
-          {sortedPosts.map((post) => {
+          {displayPosts.map((post) => {
             const { slug, date, title, description, tags } = post
             return (
               <li key={slug}>
