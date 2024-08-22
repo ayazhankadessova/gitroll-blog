d55b6f3530a7f6c88d0fccce124efeec33e1bf87
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-22 23:34:18 +0500

    fix: margins for main nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,8 +5,11 @@ import Link from 'next/link'
 export function MainNav() {
   return (
     <nav className='flex items-center space-x-4 lg:space-x-6'>
-      <Link href='/' className='mr-6 flex items-center space-x-2'>
-        <span className='font-bold text-2xl'>{siteConfig.name}</span>
+      <Link
+        href='/'
+        className='lg:ml-4 md:ml-1 mr-6 flex items-center space-x-2'
+      >
+        <h2 className='font-bold text-3xl'>{siteConfig.name}</h2>
       </Link>
     </nav>
   )
