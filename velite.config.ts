734ad3b6e99be898f53feefd241389459cebd5d6
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-23 01:10:17 +0500

    add: rehypes

--- velite.config.ts
+++ velite.config.ts
@@ -35,7 +35,20 @@ export default defineConfig({
   },
   collections: { posts },
   mdx: {
-    rehypePlugins: [],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: 'night-owl' }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          properties: {
+            className: ['subheading-anchor'],
+            ariaLabel: 'Link to section',
+          },
+        },
+      ],
+    ],
     remarkPlugins: [],
   },
 })
