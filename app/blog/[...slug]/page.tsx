91fff477181658fdd1ae4c23a584bb17b831bff8
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-22 10:57:35 +0500

    add: mdx render

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 import { posts } from '#site/content'
 import { notFound } from 'next/navigation'
+import { MDXContent } from '@/components/mdx-components'
 // import '@/styles/mdx.css'
 interface PostPageProps {
   params: {
@@ -31,6 +32,11 @@ export default async function PostPage({ params }: PostPageProps) {
   return (
     <article className='container py-6 prose dark:prose-invert max-w-3xl mx-auto'>
       <h1 className='mb-2'>{post.title}</h1>
+      {post.description ? (
+        <p className='text-xl mt-0 text-muted-foreground'>{post.description}</p>
+      ) : null}
+      <hr className='my-4' />
+      <MDXContent code={post.body} />
     </article>
   )
 }
