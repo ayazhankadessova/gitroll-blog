0a1db038482b61a983e4229f81840ff6b95e9dbc
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-26 20:40:23 +0500

    add: share icon

--- components/share-post.tsx
+++ components/share-post.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { CopyIcon, CheckIcon } from 'lucide-react'
+import { CopyIcon, CheckIcon, Share2 } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -25,45 +25,56 @@ export function SharePost({ fullLink }: SharePostProps) {
   }
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant='secondary'>Share</Button>
-      </PopoverTrigger>
-      <PopoverContent align='end' className='w-[300px]'>
-        <div className='flex flex-col space-y-2 text-left sm:text-left'>
-          <h4 className='font-semibold'>Share this page</h4>
-          <p className='text-sm text-muted-foreground'>
-            Use the link below or directly share to twitter
-          </p>
-        </div>
-        <div className='flex flex-nowrap mt-4 gap-2'>
-          <div className='grid flex-1 gap-2'>
-            <Label htmlFor='link' className='sr-only'>
-              Link
-            </Label>
-            <Input id='link' defaultValue={fullLink} readOnly className='h-9' />
-          </div>
-          <Button
-            type='button'
-            size='sm'
-            className='px-3 inline-flex items-center justify-center'
-            variant='secondary'
-            onClick={handleCopy}
-          >
-            {isCopied ? (
-              <>
-                <CheckIcon className='h-4 w-4 mr-2' />
-                <span className='hidden md:block'> Copied!</span>
-              </>
-            ) : (
-              <>
-                <CopyIcon className='h-4 w-4 mr-2' />
-                <span className='hidden md:block'>Copy Link</span>
-              </>
-            )}
+    <div className='flex'>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant='secondary'>
+            <Share2 className='mr-2 h-3 w-3' />
+            <span className='hidden md:block'>Share</span>
           </Button>
-        </div>
-      </PopoverContent>
-    </Popover>
+        </PopoverTrigger>
+        <PopoverContent align='end' className='w-[200px] md:w-[300px]'>
+          <div className='flex flex-col space-y-2 text-left sm:text-left'>
+            <h4 className='font-semibold'>Share this page</h4>
+            <p className='text-sm text-muted-foreground'>
+              Use the link below or directly share to twitter
+            </p>
+          </div>
+          <div className='flex flex-nowrap mt-4 gap-2'>
+            <div className='grid flex-1 gap-2'>
+              <Label htmlFor='link' className='sr-only'>
+                Link
+              </Label>
+              <Input
+                id='link'
+                defaultValue={fullLink}
+                readOnly
+                className='h-9'
+                type='url'
+              />
+            </div>
+            <Button
+              type='button'
+              size='sm'
+              className='px-3 inline-flex items-center justify-center'
+              variant='secondary'
+              onClick={handleCopy}
+            >
+              {isCopied ? (
+                <>
+                  <CheckIcon className='h-4 w-4 mr-2' />
+                  <span className='hidden md:block'> Copied!</span>
+                </>
+              ) : (
+                <>
+                  <CopyIcon className='h-4 w-4 mr-2' />
+                  <span className='hidden md:block'>Copy Link</span>
+                </>
+              )}
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 }
