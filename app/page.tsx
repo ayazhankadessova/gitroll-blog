3a2fe0e3ca38dea777419bfdfd47ca3b60e224e4
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-08-27 12:04:22 +0500

    fix: align home page posts to center

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { buttonVariants } from '@/components/ui/button'
-import { siteConfig } from '@/config/site'
 import { cn, sortPosts } from '@/lib/utils'
 import { posts } from '#site/content'
 import Link from 'next/link'
@@ -8,40 +7,36 @@ import { PostItem } from '@/components/post-item'
 export default function Home() {
   const latestPosts = sortPosts(posts).slice(0, 5)
   return (
-    <>
-      <section className='space-y-2 pb-6 pt-4 md:pb-10 md:mt-2'>
-        <div className='container flex max-w-screen-2xl flex-col gap-4 text-center'>
-          <div className='flex flex-col gap-4 justify-center sm:flex-row'>
-            <Link
-              href='/blog'
-              className={cn(
-                buttonVariants({ size: 'lg', variant: 'outline' }),
-                'w-full sm:w-fit'
-              )}
-            >
-              View GitRoll blog
-            </Link>
-          </div>
-        </div>
-      </section>
-      <section className='container flex max-w-screen-2xl flex-col space-y-6'>
-        <h3 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl'>
-          Latest Articles
-        </h3>
-        <ul className='flex flex-col'>
-          {latestPosts.map(
-            (post) =>
-              post.published && (
-                <li
-                  key={post.slug}
-                  className='first:border-t first:border-border'
-                >
-                  <PostItem post={post} />
-                </li>
-              )
+    <div className='container max-w-4xl py-2 lg:py-3 px-0'>
+      <div className='flex flex-col gap-4 justify-center sm:flex-row'>
+        <Link
+          href='/blog'
+          className={cn(
+            buttonVariants({ size: 'lg', variant: 'outline' }),
+            'w-full sm:w-fit'
           )}
+        >
+          View GitRoll blog
+        </Link>
+      </div>
+      <div className='flex justify-between'>
+        <h1 className='font-black text-3xl lg:text-4xl'>Latest Articles</h1>
+      </div>
+      <hr className='mt-4'></hr>
+
+      {latestPosts?.length > 0 ? (
+        <ul className='flex flex-col'>
+          {latestPosts.map((post) => {
+            return (
+              <li key={post.slug}>
+                <PostItem post={post} />
+              </li>
+            )
+          })}
         </ul>
-      </section>
-    </>
+      ) : (
+        <p>No articles yet...</p>
+      )}
+    </div>
   )
 }
